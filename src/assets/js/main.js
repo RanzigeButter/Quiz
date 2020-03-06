5e1a3851e0d6cfeec54a93bb93d6c9eebe7f675d
Tim Schneider <38572632+RanzigeButter@users.noreply.github.com>
2020-03-06 10:43:27 +0100

    added greetings

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,19 @@
 /*  ========================================================================
     # Main JavaScript
     ========================================================================  */
+
+import Greetings from './scripts/greetings';
+
+const ArrayScripts = [Greetings];
+
+const Scripts = {
+  init() {
+    if (ArrayScripts.length > 0 && ArrayScripts !== undefined) {
+      ArrayScripts.forEach(script => {
+        script.init();
+      });
+    }
+  }
+};
+
+Scripts.init();
---
+++ src/assets/js/scripts/greetings.js
@@ -0,0 +1,28 @@
+/*  ========================================================================
+    # Scripts - Greetings
+    ========================================================================  */
+
+/**
+ * Script for writing some stuff to the console.
+ */
+
+const Greetings = {
+  consoleLog(message) {
+    /* eslint-disable */
+    console.log(message);
+    /* eslint-enable */
+  },
+
+  init() {
+    this.consoleLog(`
+================================================================================
+AUTHOR: Tim Schneider
+--------------------------------------------------------------------------------
+TWITTER: @RanzigeButter
+WEBSITE: timschneider.xyz
+================================================================================
+    `);
+  }
+};
+
+export default Greetings;
